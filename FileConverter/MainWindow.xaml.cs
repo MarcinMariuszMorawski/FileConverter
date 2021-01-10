f5579cad9c50455c439291c0caa7d6b45f70627c
Marcin <marcineq95@hotmail.com>
2021-01-10 21:02:04 +0100

    some work

--- FileConverter/MainWindow.xaml.cs
+++ FileConverter/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FileConverter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void GridMainBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ButtonMinimalize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ButtonConverter_Click(object sender, RoutedEventArgs e)
+        {
+            GridPrincipal.Children.Clear();
+
+        }
+
+        private void ButtonProtection_Click(object sender, RoutedEventArgs e)
+        {
+            GridPrincipal.Children.Clear();
+
         }
     }
 }
