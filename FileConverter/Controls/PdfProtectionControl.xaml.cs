be29cb506db3ee57b0afb4907d7f3a8028cd71d7
Marcin <marcineq95@hotmail.com>
2021-01-12 22:14:37 +0100

    lock and unlock pdf

--- FileConverter/Controls/PdfProtectionControl.xaml.cs
+++ FileConverter/Controls/PdfProtectionControl.xaml.cs
@@ -1,13 +1,20 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using FileConverter.Enums;
+using FileConverter.Service;
 using MaterialDesignThemes.Wpf;
 
 namespace FileConverter.Controls
 {
     public partial class PdfProtectionControl : UserControl
     {
+        private string FileName { get; set; } = "";
         private readonly PdfProtectionTControlTypes _pdfProtectionTControlType;
+
         public PdfProtectionControl(PdfProtectionTControlTypes pdfProtectionTControlType)
         {
             InitializeComponent();
@@ -22,22 +29,68 @@ namespace FileConverter.Controls
 
         private void ButtonProtect_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(TextBoxPassword.Password))
+            if (string.IsNullOrWhiteSpace(TextBoxPassword.Password))
             {
                 IconExclamation.Visibility = Visibility.Visible;
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(TextBlockFile.Text))
+            {
+                TextBlockInfo.Text = "Please load a PDF file";
+                return;
+            }
+
+            var pdfProtectingService = new PdfProtectingService();
+
+            try
+            {
+                if (_pdfProtectionTControlType == PdfProtectionTControlTypes.Lock)
+                {
+                    var fileName = pdfProtectingService.Lock(TextBlockFile.Text, TextBoxPassword.Password,
+                        TextBoxPassword.Password);
+                    TextBlockInfo.Text = $"Successfully created locked PDF as {fileName}.";
+                }
+                else
+                {
+                    var fileName = pdfProtectingService.Unlock(TextBlockFile.Text, TextBoxPassword.Password);
+                    TextBlockInfo.Text = $"Successfully created locked PDF as {fileName}.";
+                }
+            }
+            catch (Exception exception)
+            {
+                TextBlockInfo.Text = $"Error while processing file: {exception.Message}";
             }
         }
 
         private void MainPanel_Drop(object sender, DragEventArgs e)
         {
-            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+            try
             {
-                return;
+                if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                {
+                    return;
+                }
+
+                TextBlockFile.Text = ((string[]) e.Data.GetData(DataFormats.FileDrop))?[0];
+
+
+                var extension = Path.GetExtension(TextBlockFile.Text) ?? "";
+
+                if (!extension.ToLower().Equals(".pdf"))
+                {
+                    TextBlockFile.Text = "It is not a PDF file";
+                    return;
+                }
+
+                FileName = TextBlockFile.Text;
             }
-
-            TextBlockFiles.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))?[0];
-
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Error while loading file: {exception.Message}");
+            }
         }
+
         private void TextBoxPassword_PasswordChanged(object sender, RoutedEventArgs e)
         {
             IconExclamation.Visibility = Visibility.Hidden;
---
+++ FileConverter/Service/PdfProtectingService.cs
@@ -0,0 +1,48 @@
+﻿using iText.Kernel.Pdf;
+using System.IO;
+using System.Text;
+
+namespace FileConverter.Service
+{
+    public class PdfProtectingService
+    {
+        public string Lock(string filePath, string userPassword, string ownerPassword)
+        {
+            var newFilePath = CreateNewFilePath(filePath, "-locked");
+
+            var document = new PdfDocument(new PdfReader(filePath), new PdfWriter(newFilePath,
+                new WriterProperties().SetStandardEncryption(
+                    Encoding.UTF8.GetBytes(userPassword),
+                    Encoding.UTF8.GetBytes(ownerPassword),
+                    EncryptionConstants.ALLOW_PRINTING,
+                    EncryptionConstants.ENCRYPTION_AES_128 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA
+                )));
+            document.Close();
+
+            return Path.GetFileName(newFilePath);
+        }
+
+        public string Unlock(string filePath, string ownerPassword)
+        {
+            var newFilePath = CreateNewFilePath(filePath, "-unlocked");
+
+            using var document = new PdfDocument(
+                new PdfReader(filePath, new ReaderProperties().SetPassword(Encoding.UTF8.GetBytes(ownerPassword))),
+                new PdfWriter(newFilePath)
+            );
+            document.Close();
+
+            return Path.GetFileName(newFilePath);
+        }
+
+        private static string CreateNewFilePath(string filePath, string newFileNameAddition)
+        {
+            var fileDirectory = Path.GetFullPath(Path.GetDirectoryName(filePath) ?? string.Empty);
+            var fileExtension = Path.GetExtension(filePath);
+            var fileName = Path.GetFileNameWithoutExtension(filePath);
+
+            var newFilePath = Path.Combine(fileDirectory, $"{fileName}{newFileNameAddition}{fileExtension}");
+            return newFilePath;
+        }
+    }
+}
